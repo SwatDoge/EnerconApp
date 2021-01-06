1d9161902b36bd498f648d06c1a0a2aff5cb07ec
Tycho D <97059577@st.deltion.nl>
2021-01-06 14:12:26 +0100

    roles translation

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Rollen extends Model
+class Roles extends Model
 {
     use HasFactory;
 
-    public function rol()
+    protected $table = 'roles';
+
+    public function role()
     {
         return $this->belongsToMany('App\Models\User');
     }
