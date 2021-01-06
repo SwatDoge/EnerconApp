eabc8737c68611584e8cd49ff2c690a8d2fc0aad
Tycho D <97059577@st.deltion.nl>
2021-01-06 10:42:41 +0100

    Auth Profile + Edit Auth profile

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class ProfileController extends Controller
+{
+    public function index() {
+        $user = Auth::user();
+        return view('profile.index')->with([
+            'user' => $user
+        ]);
+    }
+
+    public function update(Request $request, User $user) {
+        User::find($request->id)->update($request->validate([
+            'name' => 'required',
+            'email' => 'required'
+        ]));
+
+        return redirect()->route('pIndex');
+    }
+}
---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app', ['title' => 'Mijn gegevens'])
+
+@section('content')
+<div class="container p-5">
+    <div class="row justify-content-center">
+        <div class="col-sm-8">
+            <div class="card">
+                <h5 class="card-header">{{ $user->name }}</h5>
+                <div class="card-body">
+                    <form action="{{ route('pUpdate') }}" method="POST">
+                        <input type="hidden" name="id" value="{{ $user->id }}">
+                        @csrf @method('patch')
+                        <div class="list-group">
+                            <label for="name">Naam:</label>
+                            <input type="text" name="name" value="{{ $user->name }}" id="name" class="form-control" placeholder="{{ $user->name }}">
+                        </div>
+                        <div class="list-group">
+                            <label for="email">E-mail:</label>
+                            <input type="text" name="email" value="{{ $user->email }}" id="email" class="form-control" placeholder="{{ $user->email }}">
+                        </div>
+                        <div class="col text-center">
+                            <br>
+                            <button type="submit" class="btn btn-primary">Updaten</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
