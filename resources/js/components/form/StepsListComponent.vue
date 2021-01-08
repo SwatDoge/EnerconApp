155f78ed2afc4edd95822d7d7080e1ec9d2ba376
Stephan Muller <39221207+SwatDoge@users.noreply.github.com>
2021-01-08 11:42:03 +0100

    Fixed stlying

--- resources/js/components/form/StepsListComponent.vue
+++ resources/js/components/form/StepsListComponent.vue
@@ -9,7 +9,7 @@
                     <div class="col">Veld</div>
                     <div class="col">Omschrijving</div>
                     <div class="col">Voltooid</div>
-                    <div class="col">Datum/Tijd</div>
+                    <div class="col">Datum/Tijd</div>   
                     <div class="col">Acties</div>
                 </div>
                 <hr class="mb-3"/>
@@ -80,7 +80,6 @@
         },
         mounted(){
             this.create();
-            alert(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''));
         }
     }
 </script>
