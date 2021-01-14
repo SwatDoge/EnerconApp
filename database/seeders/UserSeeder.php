d757c36c2801340ce33d41f171d24797c2878620
maxAO2B <97050190@st.deltion.nl>
2021-01-14 13:50:48 +0100

    .

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -17,5 +19,12 @@ class UserSeeder extends Seeder
         User::factory()
             ->times(20)
             ->create();
+
+            DB::table('users')->insert([
+                ['name' => 'max',
+                 'email' => 'maxhoekstra@hotmail.com',
+                'password' => Hash::make('Welkom123'),
+                'phonenumber' => '06-21337114'],
+            ]);
     }
 }
